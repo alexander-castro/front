3125664cd1862b8d4a12ed75ac412c831f0c6072
Alexander Castro <alexander.castro01@uptc.edu.co>
2021-05-24 18:28:05 -0500

    fix

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import App from './App.vue'
 import 'materialize-css/dist/css/materialize.min.css'
 
 const app = createApp(App)
-app.config.globalProperties.API_BASE_URL = 'http://127.0.0.1:5000/api'
+app.config.globalProperties.API_BASE_URL = 'https://heart-ml.herokuapp.com/api'
 app.mount('#app')
